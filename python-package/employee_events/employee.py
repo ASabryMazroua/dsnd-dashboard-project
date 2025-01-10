1526b5fb480df03b0348230084f0eb41b2e663f0
Ahmed Sabry <a.sabrymazroua@hotmail.com>
2025-01-11 01:43:25 +0200

    Finalizing step 2

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,40 +1,42 @@
 # Import the QueryBase class
-#### YOUR CODE HERE
+from query_base import QueryBase
 
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
-#### YOUR CODE HERE
+from sql_execution import query, QueryMixin
 
-# Define a subclass of QueryBase
+# Define a subclass of QueryBase and QueryMixin
 # called Employee
-#### YOUR CODE HERE
+class Employee(QueryBase, QueryMixin):
 
     # Set the class attribute `name`
     # to the string "employee"
-    #### YOUR CODE HERE
-
+    name = "employee"
 
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
+    def names(self):
         
         # Query 3
         # Write an SQL query
         # that selects two columns 
-        # 1. The employee's full name
+        # 1. The employee's full name (concatenation of first_name and last_name)
         # 2. The employee's id
         # This query should return the data
         # for all employees in the database
-        #### YOUR CODE HERE
-    
-
+        target_query = f"""
+                        SELECT CONCAT(first_name, ' ', last_name) AS full_name, employee_id
+                        FROM {self.name}
+                        """
+        return query(target_query)
+        
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
+    def username(self, id):
         
         # Query 4
         # Write an SQL query
@@ -42,8 +44,12 @@
         # Use f-string formatting and a WHERE filter
         # to only return the full name of the employee
         # with an id equal to the id argument
-        #### YOUR CODE HERE
-
+        target_query = f"""
+                        SELECT CONCAT(first_name, ' ', last_name) AS full_name
+                        FROM {self.name}
+                        WHERE employee_id = {id}
+                        """
+        return query(target_query)
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
@@ -54,12 +60,12 @@
     # the sql query
     #### YOUR CODE HERE
     def model_data(self, id):
-
-        return f"""
-                    SELECT SUM(positive_events) positive_events
-                         , SUM(negative_events) negative_events
-                    FROM {self.name}
-                    JOIN employee_events
-                        USING({self.name}_id)
-                    WHERE {self.name}.{self.name}_id = {id}
-                """
+        target_query =  f"""
+                        SELECT SUM(positive_events) positive_events
+                            , SUM(negative_events) negative_events
+                        FROM {self.name}
+                        JOIN employee_events
+                            USING({self.name}_id)
+                        WHERE {self.name}.{self.name}_id = {id}
+                        """
+        return self.pandas_query(target_query)
--- python-package/employee_events/query_base.py
+++ python-package/employee_events/query_base.py
@@ -31,17 +31,17 @@ class QueryBase:
         # Use f-string formatting to set the name
         # of id columns used for joining
         # order by the event_date column
-        query = f"""
-                SELECT event_date, SUM(positive_events) as positive_events, SUM(negative_events) as negative_events
-                FROM {self.name}
-                JOIN employee_events
-                ON {self.name}.{id} = employee_events.{id}
-                GROUP BY event_date
-                ORDER BY event_date
-                """
+        target_query = f"""
+                        SELECT event_date, SUM(positive_events) as positive_events, SUM(negative_events) as negative_events
+                        FROM {self.name}
+                        JOIN employee_events
+                        ON {self.name}.{id} = employee_events.{id}
+                        GROUP BY event_date
+                        ORDER BY event_date
+                        """
         # I used the QueryMixin class to execute the query without inheriting
         # it as mentioned in the instructions above
-        df = QueryMixin().pandas_query(query)
+        df = QueryMixin().pandas_query(target_query)
         return df
             
     
@@ -57,12 +57,12 @@ class QueryBase:
         # with f-string formatting
         # so the query returns the notes
         # for the table name in the `name` class attribute
-        query = f"""
-                SELECT note_date, note
-                FROM notes
-                JOIN {self.name}
-                ON {self.name}.{id} = notes.{id}
-                """
-        df = QueryMixin().pandas_query(query)
+        target_query = f"""
+                        SELECT note_date, note
+                        FROM notes
+                        JOIN {self.name}
+                        ON {self.name}.{id} = notes.{id}
+                        """
+        df = QueryMixin().pandas_query(target_query)
         return df
 
